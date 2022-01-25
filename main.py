7bbb67082c9b29d21010d0d5a5fbd65a50a0578b
Target52 <gulien@me.com>
2022-01-25 20:47:03 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,46 +1,45 @@
 __winc_id__ = "ae539110d03e49ea8738fd413ac44ba8"
 __human_name__ = "files"
 
-import os, shutil, zipfile
+import os, shutil, zipfile, glob, fileinput
 
 folder = os.getcwd()
-directory = folder + '/cache'
-zip_file_path = folder + '/data.zip'
+directory = os.path.join(folder, 'cache')
+zip_file_path = os.path.join(folder, 'data.zip')
 
 def clean_cache():
     if 'cache' in os.listdir(folder):
-        shutil.rmtree(directory)
-        os.mkdir(directory)
+        filelist = [ f for f in os.listdir(directory) ]
+        for f in filelist:
+            os.remove(os.path.join(directory, f))
     else:
         os.mkdir(directory)
+
+clean_cache()
 
 def cache_zip(zip_file_path, cache_dir_path):
     with zipfile.ZipFile(zip_file_path, 'r',) as zip_ref:
         zip_ref.extractall(cache_dir_path)
 
+cache_zip(zip_file_path, directory)
 
 def cached_files():
     list_files = []
     with os.scandir(directory) as listOfEntries:
         for entry in listOfEntries:
             if entry.is_file():
-                result = os.path.join(directory, entry.name)
-                list_files.append(result)
-                
+                result = os.path.abspath(os.path.join(directory, entry))
+                list_files.append(result)     
     return list_files
 
-def find_password(cached_files):
-    files = cached_files
-    for file in files:
-        with open(file, 'r') as f:
-            read_line = f.read().split("\n")
-            for line in read_line:
-                if "password" in line:
-                    password_line = line
-                    password = password_line.split(": ")[-1]
-                else:
-                    continue
-    return password
+def find_password(files):
+        for line in fileinput.input(files):
+            if "password" in line:
+                password_line = line
+                password = password_line.split(": ")[-1]
+            else:
+                continue
+        return password
 
 print("The password is: ")
 print(find_password(cached_files()))
